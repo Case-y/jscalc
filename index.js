f6c3fb8b18a2f1935eb7e4c2bbe47443616785d6
Casey Takeda <kaze@berkeley.edu>
2016-04-10 22:10:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,23 +14,21 @@ $(document).ready(function()  {
   // numbers // 
   var value = [];
   var j;
-  var num;
+  var num = "";
   var adder;
   var temp;
   var lastdigit;
   var hold;
-  
-  $(".b_").click(function() {
-    $(".inside").text("It's Casey!");
-  });
+  var decimal = false;
   
   $("button").click(function() { 
 
   entry = $(this).html();
     
   for (i = 0; i < entry.length; i++) {
-    if (entry[i] == '/' || entry[i] == '*' || entry[i] == '-' || entry[i] == '+' || entry[i] == '=' || entry[i] == 'c') {
-      operation = entry[i];   // gets the procedure
+    if (entry[i] == '/' || entry[i] == '*' || entry[i] == "." || entry[i] == '-' || entry[i] == '+' || entry[i] == '=' || entry[i] == 'c') {
+      operation = entry[i];
+      // gets the procedure
     }   
     if (entry[i] == '0' || entry[i] == '1' || entry[i] == '2' || entry[i] == '3' || entry[i] == '4' || entry[i] == '5' || entry[i] == '6' || entry[i] == '7' || entry[i] == '8' || entry[i] == '9') {
       operation = parseInt(entry[i]);  // gets the number
@@ -39,9 +37,11 @@ $(document).ready(function()  {
   
     
   if (operation == '/' || operation == '*' || operation == '-' || operation == '+') { // procedure
-      if (typeof num === 'number') {
-      procedures.push(num);
+      if (typeof Number(num) === 'number') {
+      procedures.push(Number(num));
       value = [];
+        num = ""
+        decimal = false;
       }
       if (procedures.length >= 1) {
       procedures.push(operation);
@@ -50,14 +50,14 @@ $(document).ready(function()  {
     
   else if (operation == '=') {        // calculation
     if (procedures.length == 0) {
-      $(".inside").text("Nothing. Void.");
+      $(".inside").text("Enter...");
       total = 0;
-      num = 0;
-      value = [];
+      num = "";
+      // value = [];
       procedures = [];
     }
     else {
-    procedures.push(num);  
+    procedures.push(Number(num));  
     total = procedures[0];
     for (k = 1; k < procedures.length; k++) {
         target = procedures[k];
@@ -82,42 +82,48 @@ $(document).ready(function()  {
     
     $(".inside").text(total);
     total = 0;
-    num = 0;
+    num = "";
     value = [];
     procedures = [];
+      decimal = false;
     }
   }
     
   else if (operation == 'c') {   // clearing
     $(".inside").text("clear");
-    num = 0;
+    num = "";
     value = [];
     procedures = [];    
   }
   
-  else if (num > 100000000000) {  // Calculation too advanced
+  else if (Number(num) > 100000000000) {  // Calculation too advanced
     $(".inside").text("Max Reached");
-    num = 0;
+    num = "";
     value = [];
   }
   
     
-  else if (typeof operation === 'number') {  // number
-    num = operation;
-    value.push(num);
-    if (value.length == 2) {
-        temp = value[0];
-        hold = 1;
-        while (temp > 0) {          
-            lastdigit = temp % 10;
-            if (lastdigit != 0) {
-              num = (Math.pow(10, hold) * lastdigit) + num;
-            }
-            temp = Math.floor(temp / 10);
-            hold += 1;
-        }
-        value = [num];
+  else if (typeof operation === 'number' || operation == "." ) {  // number or decimanl
+    if (operation == "." && !decimal) {
+      num += ".";
+      decimal = true;
+    } else if (!decimal || typeof operation === 'number') {
+      num += operation;
     }
+    // value.push(num);
+    // if (value.length == 2) {
+    //     temp = value[0];
+    //     hold = 1;
+    //     while (temp > 0) {          
+    //         lastdigit = temp % 10;
+    //         if (lastdigit != 0) {
+    //           num = (Math.pow(10, hold) * lastdigit) + num;
+    //         }
+    //         temp = Math.floor(temp / 10);
+    //         hold += 1;
+    //     }
+    //     value = [num];
+    // }
     
     $(".inside").text(num);
   }
